6f40e8274504e7de6f8f9002e56ed7e8d71fe786
iamshubh22 <sgshubham03@gmail.com>
2017-05-01 02:59:16 +0530

    Update

--- src/Main.purs
+++ src/Main.purs
@@ -73,7 +73,8 @@ project3Dto2D (Point3D p) (Angle3D ang) =
 		Point3D {x:xRotQzQxQy, y:yRotQzQxQy, z:p.z}
 
 drawCube :: forall e. Context2D -> Eff (canvas :: CANVAS | e) Unit
-drawCube ctx =
+drawCube ctx = do
+	ctx <- setFillStyle "#000000" ctx
 	fillPath ctx do
 		ctx <- moveTo ctx 100.0 100.0
 		ctx <- lineTo ctx 200.0 200.0
@@ -95,4 +96,4 @@ main = void $ unsafePartial do
 	ctx <- setFillStyle "#0000FF" ctx
 	--ctx <- fillRect {x: 0.0, y: 0.0, w: 500.0, h: 500.0} ctx
 	drawCube ctx
-	log("helloworls")
+	log("working")
